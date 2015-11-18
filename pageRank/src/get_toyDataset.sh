25ef72f2b8b746233a3d4a2e760c74f605c0487a
williamdotyang <williamdotyang@gmail.com>
2015-11-17 20:44:52 -0600

    shell script for screening out the toy dataset.

---
+++ pageRank/src/get_toyDataset.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#grep "WI\tCardiology" ../data/PARTD_PRESCRIBER_PUF_NPI_DRUG_13.tab > ../data/wi_cardi.tab
+#grep "WI\tInternal Medicine" ../data/PARTD_PRESCRIBER_PUF_NPI_DRUG_13.tab > ../data/wi_im.tab
+grep "WI\tDermatology" ../data/PARTD_PRESCRIBER_PUF_NPI_DRUG_13.tab > ../data/wi_dem.tab
+
+#grep "NC\tCardiology" ../data/PARTD_PRESCRIBER_PUF_NPI_DRUG_13.tab > ../data/nc_cardi.tab
+#grep "NC\tInternal Medicine" ../data/PARTD_PRESCRIBER_PUF_NPI_DRUG_13.tab > ../data/nc_im.tab
+grep "NC\tDermatology" ../data/PARTD_PRESCRIBER_PUF_NPI_DRUG_13.tab > ../data/nc_dem.tab
+
+grep "NY\tCardiology" ../data/PARTD_PRESCRIBER_PUF_NPI_DRUG_13.tab > ../data/ny_cardi.tab
+grep "NY\tInternal Medicine" ../data/PARTD_PRESCRIBER_PUF_NPI_DRUG_13.tab > ../data/ny_im.tab
+grep "NY\tDermatology" ../data/PARTD_PRESCRIBER_PUF_NPI_DRUG_13.tab > ../data/ny_dem.tab
