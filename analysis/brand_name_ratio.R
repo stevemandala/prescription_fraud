f5941dcdc34770cc04e28e0b9e0e23f6adc32c4c
WangZongyan <zwang634@wisc.edu>
2015-11-24 10:04:38 -0600

    merge two data.frame, the T- groups result can be find with hsd.out$group.id

--- analysis/brand_name_ratio.R
+++ analysis/brand_name_ratio.R
@@ -2,7 +2,7 @@ library(dplyr)
 library(agricolae)
 
 # Load data
-wi_dem2 = read.csv("wi_dem2.tab", sep="\t", header=FALSE)
+wi_dem2 = read.csv("F:/Academic/Stat 992/group project/toy_data/wi_dem2.tab", sep="\t", header=FALSE)
 hospitals = unique(wi_dem2[, 20])
 
 grouped_by_hospital = wi_dem2 %>% 
@@ -19,12 +19,12 @@ hist(bg_ratios$bg_ratio)
 
 ## Create an treatment record for HSD test
 data <-grouped_by_hospital
-data <- cbind(data,id=factor(data$V20,labels=1:length(unique(data$V20))))
+data <- cbind(data,trt=factor(data$V20,labels=1:length(unique(data$V20))))
 ## Do LSD & HSD test
-model <- aov(as.numeric(as.vector(drug_name)==as.vector(generic_name)) ~ id, 
+model <- aov(as.numeric(as.vector(drug_name)==as.vector(generic_name)) ~ trt, 
              data = data)
-lsd.out <- LSD.test(model, "id", p.adj = "bonferroni")
-hsd.out <- HSD.test(model,"id", group = T)
+lsd.out <- LSD.test(model, "trt", p.adj = "bonferroni")
+hsd.out <- HSD.test(model,"trt", group = T)
 
 bar.group(lsd.out$groups,ylim=c(0,45),density=4,border="blue")
 bar.group(hsd.out$groups,ylim=c(0,45),density=4,border="blue")
@@ -34,5 +34,8 @@ hsd.out$groups
 
 ## You can match the group number with hospital with the data we use
 id.group <- data %>% 
-  select(V20, id) %>%
+  select(V20, trt) %>%
   unique()
+hsd.out$groups.id <- merge(id.group, hsd.out$groups, by = "trt")
+hsd.out$groups.id
+
