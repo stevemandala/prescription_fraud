29efb0c59a8c9672239fc80989c5af6a60d8ae14
patron <patron@dyn-72-33-184-125.uwnet.wisc.edu>
2015-11-23 19:45:10 -0600

    extended some brandname/ration codes

--- analysis/brand_name_cnt_ratio.R
+++ analysis/brand_name_cnt_ratio.R
@@ -1,20 +1,29 @@
 #Note: While the metric of brand_name != generic_name is generally affective, there are some 
-# outliers to consider, such as when a substring of the total name is used
+# outliers to consider, such as when a substring of the total name is used as "brand name"
 
 library(dplyr)
-wi_dem2 = read.csv("wi_dem2.tab", sep="\t", header=FALSE)
+phy_drugs = read.csv("wi_cardi2.tab", sep="\t", header=FALSE)
 hospitals = unique(wi_dem2[, 20])
 
-grouped_by_hospital = wi_dem2 %>% 
+grouped_by_hospital = phy_drugs %>% 
   group_by(V20) %>%
   select(drug_name = V8, generic_name = V9, total_claim_cnt = V11) 
 
 #Calculates ratios based on number of brand name vs total claims
-bg_ratios = summarise(grouped_by_hospital, 
+hosp_bg_ratios = summarise(grouped_by_hospital, 
       bg_ratio = (sum(as.vector(total_claim_cnt[as.vector(drug_name) != as.vector(generic_name)]))/sum(as.vector(total_claim_cnt)))
       )
    
-hist(bg_ratios$bg_ratio)
+hist(hosp_bg_ratios$bg_ratio)
 
+grouped_by_physician = phy_drugs %>% 
+  group_by(V1) %>%
+  select(drug_name = V8, generic_name = V9, total_claim_cnt = V11) 
 
+#Calculates ratios based on number of brand name vs total claims
+phy_bg_ratios = summarise(grouped_by_physician, 
+                      bg_ratio = (sum(as.vector(total_claim_cnt[as.vector(drug_name) != as.vector(generic_name)]))/sum(as.vector(total_claim_cnt)))
+)
+
+hist(phy_bg_ratios$bg_ratio)
 
--- processing/wi_data.R
+++ processing/wi_data.R
@@ -1,3 +1,41 @@
 library(igraph)
 library(data.table)
 
+b= c(rep("character", 6),rep("factor",4), "numeric", rep("factor",6), "character", "character", "character", "numeric", rep("character",2), "factor", "character", "factor", "character", rep("character", 10), rep("factor", 6))
+DT = fread("../data/National_Downloadable_File.csv",colClasses = b)
+names(DT)[1] <- c("NPI")
+setkey(DT, NPI)
+DT_hosp = DT[`State`=="WI" & `Organization DBA name`=="UW MEDICAL FOUNDATION"]
+
+phy_drug_main = fread("PARTD_PRESCRIBER_PUF_NPI_DRUG_13.tab")
+# phy_drug = phy_drug_main[`NPPES_PROVIDER_STATE`=='WI' 
+#                     & `NPPES_PROVIDER_CITY`== 'MADISON']
+#                    # & (`SPECIALTY_DESC`=="Internal Medicine" | `SPECIALTY_DESC`=="Cardiology")]
+
+phy_drug = phy_drug_main[`NPI` %in% DT_hosp$NPI]
+
+Et = fread("physician-referrals-2014-days365.txt",sep = ",",  colClasses = c("character", "character","numeric", "numeric", "numeric"))
+setkey(Et,V1)
+
+Emad = Et[V1 %in% unique(phy_drug$NPI)]
+Emad = Emad[V2 %in% unique(phy_drug$NPI)]
+
+el=as.matrix(Emad)[,1:2] #igraph needs the edgelist to be in matrix format
+g=graph.edgelist(el,directed = F) # this creates a graph.
+# original graph is directed.  For now, ignore direction.
+g = simplify(g)
+loc  = layout.fruchterman.reingold(g)
+plot(g, vertex.label= NA, layout=loc, vertex.color = as.factor(phy_drug[V(g), mult="first"]$SPECIALTY_DESC))
+
+
+grouped_by_physician = phy_drugs %>% 
+  group_by(NPI) %>%
+  select(drug_name = DRUG_NAME, generic_name = GENERIC_NAME, total_claim_cnt = TOTAL_CLAIM_COUNT) 
+
+#Calculates ratios based on number of brand name vs total claims
+phy_bg_ratios = summarise(grouped_by_physician, 
+                          bg_ratio = (sum(as.vector(total_claim_cnt[as.vector(drug_name) != as.vector(generic_name)]))/sum(as.vector(total_claim_cnt)))
+)
+
+hist(phy_bg_ratios$bg_ratio)
+
