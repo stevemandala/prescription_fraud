08b25535f8f0ce3a71a3e18fd562ef8f9897ed52
WangZongyan <zwang634@wisc.edu>
2015-12-03 10:47:46 -0600

    The new drug usage over States. Plot has been saved as "newDrug_Usage_plot_state.png".
    The next version will add brand_name/generic_name ratio

---
+++ analysis/newDrug_States.R
@@ -0,0 +1,43 @@
+##Where those physicians' from?
+
+#####################################################
+# Load packages
+require(downloader)
+require(data.table)
+require(ggplot2)
+require(plyr)
+require(dplyr)
+
+# Download and Load PartD_newDrug.csv
+setwd("F:/Academic/Stat 992/group project/prescription_fraud")
+partD.new <- fread("PartD_newDrug.csv")
+names(partD.new)
+#############################################################
+
+
+# Where are those physicians from? Usage
+
+#usage <- partD.new %>% ddply(~NPPES_PROVIDER_STATE, function(x){length(x$NPI)})
+usage <- partD.new %>%
+  group_by(NPPES_PROVIDER_STATE) %>% 
+  summarise(usage = length(.$NPI))
+
+# Arrange and reorder the data
+usage <- usage %>%
+  dplyr::mutate(state.reorder = reorder(x = NPPES_PROVIDER_STATE,
+                                        X = usage, FUN = min)) %>%
+  dplyr::arrange(., desc(usage))
+
+
+
+usagePlot <- ggplot(usage, aes(x = state.reorder, y = usage)) +
+  geom_point(aes(color = state.reorder))
+cat("The total new drug usage over States \n")
+print(usagePlot)
+## Save the plot as "newDrug_Usage_plot_state.png"
+ggsave(filename = "newDrug_Usage_plot_state.png", plot = usagePlot, path = ".",  
+       width = 10, height = 10, dpi = 600)
+
+
+
+
