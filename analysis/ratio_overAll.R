7ee0579775fc0cfb70dd24dcba2eda1d979cef4c
WangZongyan <zwang634@wisc.edu>
2015-12-04 16:54:55 -0600

    the brand/generic ratio over States, the weight stands for total_claim_account

---
+++ analysis/ratio_overAll.R
@@ -0,0 +1,36 @@
+# This document create a map for the ratio of brandName and generic name over States.
+# This document also create a map for ratio counts the weight of column TOTAL_CLAIM_COUNT  
+
+# Load package
+require(data.table)
+require(plyr)
+require(dplyr)
+
+# Load data
+
+partD <- fread("../PartD_Prescriber_PUF_NPI_DRUG_13/PARTD_PRESCRIBER_PUF_NPI_DRUG_13.tab",
+               sep = "\t")
+
+
+# function for the ratio
+#' @param x: data from one particular State
+#' @param weight: count the weight of column TOTAL_CLAIM_COUNT 
+
+ratio <- function(x, weight = F){
+  if(weight){
+    x$weight = x$TOTAL_CLAIM_COUNT
+  }else{
+    x <- x %>%
+      dplyr::mutate(weight = 1)
+  } 
+  x <- x %>% 
+    summarize(bNotG = sum((DRUG_NAME != GENERIC_NAME)*weight)/ sum(weight))
+}
+
+ratio_withoutWeight <- partD %>% ddply("NPPES_PROVIDER_STATE", function(x){ratio(x)})
+ratio_withWeight <- partD %>% ddply("NPPES_PROVIDER_STATE", 
+                                    function(x){ratio(x, weight = T)})
+
+# Output those two data frame in "ratio_withoutWeight.csv" and "ratio_withWeight.csv"
+write.csv(ratio_withoutWeight, file = "ratio_withoutWeight.csv", row.names = F)
+write.csv(ratio_withWeight, file= "ratio_withWeight.csv", row.names = F)
