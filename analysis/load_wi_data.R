991ec88170e8b2728c59eaad2a4d63c7cde33a56
patron <patron@dyn-72-33-226-184.uwnet.wisc.edu>
2015-12-05 16:55:13 -0600

    started spectral clustering, visualizing clusters on graph

---
+++ analysis/load_wi_data.R
@@ -0,0 +1,26 @@
+# Data Parsing
+Et = fread("physician-referrals-2014-days365.txt",sep = ",",  colClasses = c("character", "character","numeric", "numeric", "numeric"))
+setkey(Et, V1)
+head(Et)
+b= c(rep("character", 6),rep("factor",4), "numeric", rep("factor",6), "character", "character", "character", "numeric", rep("character",2), "factor", "character", "factor", "character", rep("character", 10), rep("factor", 6))
+DT = fread("National_Downloadable_File.csv",colClasses = b)
+names(DT)[1] <- c("NPI")
+setkey(DT, NPI)
+rm(b)
+wi = DT[State == "WI"]
+tmp = Et[unique(wi$NPI)]  # so cool! and fast!
+Ewi = tmp[complete.cases (tmp)]  #lots of NA's.  Have not inspected why.
+
+write.table(wi, file = "wi_DT.txt", sep=",", col.names = T, row.names = F)
+write.table(Ewi, file = "wi_Et.txt", sep=",", col.names = T, row.names = F)
+
+
+b= c(rep("character", 6),rep("factor",4), "numeric", rep("factor",6), "character", "character", "character", "numeric", rep("character",2), "factor", "character", "factor", "character", rep("character", 10), rep("factor", 6))
+wi = fread("wi_DT.txt",colClasses = b)
+setkey(wi, NPI)
+Ewi = fread("wi_Et.txt",sep = ",",  colClasses = c("character", "character","numeric", "numeric", "numeric"))
+setkey(Ewi, V1)
+
+wi = wi[City == "MADISON"]
+tmp = Ewi[unique(wi$NPI)]  # so cool! and fast!
+Ewi = tmp[complete.cases (tmp)]  #lots of NA's.  Have not inspected why.
--- analysis/spectral_clustering.R
+++ analysis/spectral_clustering.R
@@ -6,42 +6,28 @@ library(data.table)
 library(rARPACK)
 library(zipcode)
 
-# Data Parsing
-Et = fread("physician-referrals-2014-days365.txt",sep = ",",  colClasses = c("character", "character","numeric", "numeric", "numeric"))
-setkey(Et, V1)
-head(Et)
-b= c(rep("character", 6),rep("factor",4), "numeric", rep("factor",6), "character", "character", "character", "numeric", rep("character",2), "factor", "character", "factor", "character", rep("character", 10), rep("factor", 6))
-DT = fread("National_Downloadable_File.csv",colClasses = b)
-names(DT)[1] <- c("NPI")
-setkey(DT, NPI)
-rm(b)
-wi = DT[State == "WI"]
-tmp = Et[unique(mad$NPI)]  # so cool! and fast!
-Emad = tmp[complete.cases (tmp)]  #lots of NA's.  Have not inspected why.
-el=as.matrix(Emad)[,1:2] #igraph needs the edgelist to be in matrix format
+if (FALSE) {
+  source("load_wi_data.R")
+}
+
+el=as.matrix(Ewi)[,1:2] #igraph needs the edgelist to be in matrix format
 g=graph.edgelist(el,directed = F) # this creates a graph.
-# original graph is directed.  For now, ignore direction.
 
 set.seed(1)
-rm(list = ls())
-load(url("http://pages.stat.wisc.edu/~karlrohe/netsci/data/zipA.RData"))
-dim(A)
-
-g = graph.adjacency(A,mode = "directed", weighted = T)
-clust = clusters(g, mode = "weak")
-table(clust$csize)
 
 #Coreness pruning (optional)
 core = graph.coreness(g, mode = "all")
-hist(core)
 g = induced.subgraph(g,vids = V(g)[core>=5])
-clust = clusters(g, mode = "weak")
-table(clust$csize)
+#clust = clusters(g, mode = "weak")
+#table(clust$csize)
 
 A = get.adjacency(g)
 vec = eigs(A,k = 50)  # So fast!
 
-data(zipcode)
+locs = layout.auto(g)
+plot(g, vertex.label = NA, vertex.color = as.factor(vec$vec[,3]>0), layout = locs)
+
+sdata(zipcode)
 zipcode = as.data.table(zipcode); setkey(zipcode, zip)
 loc = zipcode[rownames(A), c("longitude", "latitude"), with  =F]
 for(i in 2:10) plot(loc, col=as.factor(vec$vec[,i]>0),
