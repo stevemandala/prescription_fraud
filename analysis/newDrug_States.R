ee5ca68306d3cc339fbd1834512fdfbec65a56f1
WangZongyan <zwang634@wisc.edu>
2015-12-04 19:17:50 -0600

    output newdrug plot and heatmap. The new data set "combine_drugList" merge the rows have the same "drugName". And "partD_mergeWith_drug.list.csv" is the file contains all the information about partD.new and new drug list. They are the output of Combine_PartD_drugList.

--- analysis/newDrug_States.R
+++ analysis/newDrug_States.R
@@ -38,6 +38,64 @@ print(usagePlot)
 ggsave(filename = "newDrug_Usage_plot_state.png", plot = usagePlot, path = ".",  
        width = 10, height = 10, dpi = 600)
 
+#####################################################
+## heatmap
+# get the abbreviation and full name online
+URL <- "http://state.1keydata.com/state-abbreviations.php"
+tables = readHTMLTable(URL, stringsAsFactors = FALSE)
+list <- tables[[2]]
+list <- data.frame(full = c(list$V1, list$V3), abb = c(list$V2, list$V4))
+state <- list[-27, ][-1, ]
+row.names(state) <- 1 : length(state[,1])
 
+## drop the unnecessary areas
+dropArea <- function(x, state = state){
+  x <- x[which(x$NPPES_PROVIDER_STATE %in% state$abb),]
+  row.names(x) <- 1 : length(x$usage)
+  return(x)
+}
 
+## Make every alphabet to lower case
+low <- function(x, state = state){
+  index <- grep(pattern = as.character(x$NPPES_PROVIDER_STATE), state$abb)
+  x$region <- state$full[index]
+  x$region <- tolower(x$region)
+  return(x)
+}
 
+Low <- function(data, state = state){
+  y <- data %>% ddply(~NPPES_PROVIDER_STATE, function(x){low(x, state)})
+  return(y)
+}
+
+# Get Average Latitude and Longitude for US States
+location <- read.csv("state_latlon.csv")
+names(location)[1] <- c("NPPES_PROVIDER_STATE")
+
+loc <- function(x, location = location){
+  x <- merge(x, location, by = "NPPES_PROVIDER_STATE")
+  return(x)
+}
+heatmap_new <- function(x){
+  states <- map_data("state")
+  map.df <- merge(states,x, by="region", all.x=T)
+  map.df <- map.df[order(map.df$order),]
+  myPlot <- ggplot(map.df, aes(x=long,y=lat,group=group))+
+    geom_polygon(aes(fill= usage))+
+    geom_path()+ 
+    scale_fill_gradientn(colours=rev(heat.colors(20)),na.value="grey90")+
+    geom_text(aes(x=longitude, y=latitude, label=NPPES_PROVIDER_STATE), 
+              size=3) +
+    coord_map()
+  myPlot
+  return(myPlot)
+}
+
+## plot
+y <- dropArea(usage, state)
+y <- Low(y, state = state)
+y <- loc(y, location)
+heatmap_new <- heatmap_new(y)
+print(heatmap_new)
+ggsave(filename = "heatmap_new.png", plot = last_plot(), path = ".",  
+       width = 10, height = 10, dpi = 600)
