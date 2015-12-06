1c91ed992a40f3e9d9a9c69a69d4bb40e06ce328
WangZongyan <zwang634@wisc.edu>
2015-12-06 01:11:09 -0600

    This file select the NPI of Internal Medicine physicians in referral data, the code uses the last row information dictionary. Sort() is not included in consider that reading the whole data into memory may cause crash

--- analysis.py/internalReferral.py
+++ analysis.py/internalReferral.py
@@ -11,6 +11,7 @@ kk = []
 Referral =[]
 dic = {}   # dictionary of referral npi
 not_dic = {}  # dictionary of referral npi not Internal medicine
+
 # filter the npi is valid
 def isInt(value):
   try:
@@ -31,24 +32,33 @@ with open("F:/Academic/Stat 992/group project/prescription_fraud/analysis.py/Int
 print("Reading NPI completely")
 
 #### Search NPI in referral data
-with open("internal_referral.csv", "a") as inter_refer:
+with open("internal_referral1.csv", "a") as inter_refer:
     writer = csv.writer(inter_refer)
     with open("F:/Academic/Stat 992/group project/physician_referrals13-14_inPARTD.csv") as ref:
         kk.extend(ref.readline() for j in ref)
+        last_npi = 0 # mark this row has the same npi as last row
+        saveToFile = False
         for j in range(len(kk)):
             referral = kk[j].strip().split(",")
-            if isInt(referral[0]):
-                if int(referral[0]) in not_dic:
-                    continue
+            if int(referral[0]) == last_npi:
+               if saveToFile:
+                    writer.writerows([referral])
+               continue
+            else:
+                last_npi = int(referral[0])
                 if int(referral[0]) in dic:   # referral can be found in previous
                     writer.writerows([referral])
+                    saveToFile = True
+                    continue
+
+                if referral[0] in NPI:
+                    writer.writerows([referral])
+                    saveToFile = True
+                    dic[int(referral[0])] = referral[0]
                 else:
-                    if referral[0] in NPI:
-                        writer.writerows([referral])
-                        dic[int(referral[0])] = referral[0]
-                    else:
-                        not_dic[int(referral[0])] = referral[0]
+                    # not_dic[int(referral[0])] = referral[0]
+                    saveToFile = False
 
 print("the referral data contains the physician who gives referral is Internal Medicine "
-      "is written to 'internal_referral.csv'")
+      "is written to 'internal_referral1.csv'")
 
