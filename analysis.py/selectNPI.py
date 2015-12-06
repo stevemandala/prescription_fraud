2ce47b818aa07a4a2969c68c690220505de4e588
WangZongyan <zwang634@wisc.edu>
2015-12-05 21:39:37 -0600

    This file select the NPI and row data in Internal Medicine

--- analysis.py/selectNPI.py
+++ analysis.py/selectNPI.py
@@ -6,7 +6,7 @@ import csv
 col = []
 colnames = []
 lines = []
-NPI = []
+NPI = {}
 
 # filter the npi is valid
 def isInt(value):
@@ -15,33 +15,35 @@ def isInt(value):
     return True
   except:
     return False
+with open("InternalNPI.csv", "a") as iex:
+    writer1 = csv.writer(iex)
+    writer1.writerows([["NPI"]])
+    with open("internal.csv", "a") as inter:
+        writer2 = csv.writer(inter)
+        with open("F:/Academic/Stat 992/group project/PartD_Prescriber_PUF_NPI_DRUG_13/"
+               "PARTD_PRESCRIBER_PUF_NPI_DRUG_13.tab") as f:
+#        with open("F:/Academic/Stat 992/group project/toy_data/wi_dem2.tab") as f:
 
-with open("internal.csv", "a") as ex:
-    writer = csv.writer(ex)
-    with open("F:/Academic/Stat 992/group project/PartD_Prescriber_PUF_NPI_DRUG_13/"
-          "PARTD_PRESCRIBER_PUF_NPI_DRUG_13.tab") as f:
-#    with open("F:/Academic/Stat 992/group project/toy_data/wi_dem2.tab") as f:
-
-        col.extend(f.readline() for i in range(1))
-        colnames.append(col[0].strip().split("\t"))
-        writer.writerows(colnames)
-        lines.extend(f.readline() for i in f)
-        for i in range(len(lines)):
-#        print(lines[i])
-            if(str.find(lines[i], "Internal Medicine") != -1):
-                k = lines[i].strip().split("\t")
-                npi = k[0]
-                if(isInt(npi)):
-                    internal = []
-                    NPI.append([npi])
-                    internal.append(k)
-                    writer.writerows(internal)
+            col.extend(f.readline() for i in range(1))
+            colnames.append(col[0].strip().split("\t"))
+            writer2.writerows(colnames)
+            lines.extend(f.readline() for i in f)
+            for i in range(len(lines)):
+#            print(lines[i])
+                if(str.find(lines[i], "MI") != -1):
+                    k = lines[i].strip().split("\t")
+                    npi = k[0]
+                    if(isInt(npi)):
+                        npi = int(npi)
+                        internal = []
+                        internal.append(k)
+                        writer2.writerows(internal)
+                        if not npi in NPI:
+                            NPI[npi] = npi
+                            writer1.writerows([[npi]])
 
 
-with open("InternalNPI.csv", "w") as iex:
-    writer = csv.writer(iex)
-    writer.writerows([["NPI"]])
-    writer.writerows(NPI)
+
 print("The file has been written to 'internal.csv', it has ", str(len(NPI)), "observations")
 print("The file has been written to 'InternalNPI.csv'")
 
