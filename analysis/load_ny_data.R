72c17d4bb7f38b81255f251ad3e9cdf8e3519c80
patron <patron@wifi-ls-73-223.resnet.wisc.edu>
2015-12-07 23:27:05 -0600

    some more work, peer ration comparisons

--- analysis/load_ny_data.R
+++ analysis/load_ny_data.R
@@ -9,18 +9,18 @@ DT = fread("National_Downloadable_File.csv",colClasses = b)
 names(DT)[1] <- c("NPI")
 setkey(DT, NPI)
 rm(b)
-wi = DT[State == "WI"]
+wi = DT[State == "NY"]
 tmp = Et[unique(wi$NPI)]  # so cool! and fast!
 Ewi = tmp[complete.cases (tmp)]  #lots of NA's.  Have not inspected why.
 
-write.table(wi, file = "wi_DT.txt", sep=",", col.names = T, row.names = F)
-write.table(Ewi, file = "wi_Et.txt", sep=",", col.names = T, row.names = F)
+write.table(wi, file = "ny_DT.txt", sep=",", col.names = T, row.names = F)
+write.table(Ewi, file = "ny_Et.txt", sep=",", col.names = T, row.names = F)
 
 
 b= c(rep("character", 6),rep("factor",4), "numeric", rep("factor",6), "character", "character", "character", "numeric", rep("character",2), "factor", "character", "factor", "character", rep("character", 10), rep("factor", 6))
-wi = fread("wi_DT.txt",colClasses = b)
+wi = fread("ny_DT.txt",colClasses = b)
 setkey(wi, NPI)
-Ewi = fread("wi_Et.txt",sep = ",",  colClasses = c("character", "character","numeric", "numeric", "numeric"))
+Ewi = fread("ny_Et.txt",sep = ",",  colClasses = c("character", "character","numeric", "numeric", "numeric"))
 setkey(Ewi, V1)
  
 wi = wi[City == "MADISON"]
