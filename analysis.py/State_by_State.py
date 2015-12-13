602eca23f5c348b69be3d8994d52c03ade6a0729
WangZongyan <zwang634@wisc.edu>
2015-12-13 15:47:02 -0600

    This file calculate the b/g ratio for each physician for whole data(2.7GB partD), the result is a 93 MB csv file, can be used to calculate ANOVA, HSD, and doing classification. The first column of the output is the b/g, the second column is the b/g with the count of total_claim, the total drug cost and total day supply is the sum up value. Use fread() to open it in R

--- analysis.py/State_by_State.py
+++ analysis.py/State_by_State.py
@@ -14,24 +14,29 @@ def isInt(value):
 
 
 
-# rt = []
+#rt = []
 data = {}
 data_new = []
-with open("NPI_bg.csv", "w") as output:
+with open("NPI_bg.csv", "w", newline = "") as output:
     writer = csv.writer(output)
     with open("F:/Academic/Stat 992/group project/"
               "PartD_Prescriber_PUF_NPI_DRUG_13/PARTD_PRESCRIBER_PUF_NPI_DRUG_13.tab") as file:
     #with open("F:/Academic/Stat 992/group project/toy_data/wi_dem2.tab") as file:
-        # rt.extend(file.readlines())
+        #rt.extend(file.readline() for i in range(5))
         ### Write the file
+        #for i in rt:
         for rt in file:
             rows = rt.strip().split("\t")
             if(isInt(rows[0])):
                 if rows[0] in data:  # if the physician is in the dictionary created
                     data[rows[0]][1] += 1
+                    data[rows[0]][15] += float(rows[11])  ## total day supply
+                    data[rows[0]][16] += float(rows[12])  ## total drug cost
                 else: # create a new dictionary for this physician
                     data[rows[0]] = [0, 1, 0, int(rows[10])] # add total_claim to general count
                     data[rows[0]].extend(rows)
+                    data[rows[0]][15] = float(data[rows[0]][15])
+                    data[rows[0]][16] = float(data[rows[0]][16])
                 if rows[7] == rows[8]: # physician use a brand_name
                     data[rows[0]][0] += 1
                     data[rows[0]][2] += int(rows[10])   ## add total_claim to brand-name count
@@ -56,7 +61,7 @@ with open("NPI_bg.csv", "w") as output:
         data[i].pop(11) # delete the generic_name column
         data[i][0] = data[i][0]/data[i][1]  # calculate b/g
         data[i][1] = data[i][2]/data[i][3]  # calculate b/g with total_claim
-        data[i][12] = data[i][3]
+        data[i][12] = data[i][3]  ## Total claim
         data[i].pop(3)
         data[i].pop(2)
         writer.writerows([data[i]])
