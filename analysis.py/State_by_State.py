26cccbae1222e8ffb127ca1a471c0b58dd66e329
WangZongyan <zwang634@wisc.edu>
2015-12-12 20:33:08 -0600

    This file calculate the b/g ratio for each physician for whole data(2.7GB partD), the result can be used to calculate ANOVA, HSD, and doing classification. The first column of the output is the b/g, the second column is the b/g with the count of total_claim

--- analysis.py/State_by_State.py
+++ analysis.py/State_by_State.py
@@ -3,37 +3,62 @@ __author__ = 'zongyan wang'
 import csv
 import string
 
+
+# filter the npi is valid
+def isInt(value):
+  try:
+    float(value)
+    return True
+  except:
+    return False
+
+
+
 rt = []
 data = {}
 data_new = []
-with open("F:/Academic/Stat 992/group project/"
-          "PartD_Prescriber_PUF_NPI_DRUG_13/PARTD_PRESCRIBER_PUF_NPI_DRUG_13.tab") as file:
-#with open("F:/Academic/Stat 992/group project/toy_data/wi_dem2.tab") as file:
-    rt.extend(file.readline() for i in file)
-    for i in range(len(rt)):
-        rows = rt[i].strip().split("\t")
-        if(len(rows) > 8):
-            if rows[0] in data:  # if the physician is in the dictionary created
-                data[rows[0]][1] += 1
-            else: # create a new dictionary for this physician
-                data[rows[0]] = [0, 1]
-                data[rows[0]].extend(rows)
-            if rows[7] == rows[8]: # physician use a brand_name
-                data[rows[0]][0] += 1
-
-print("The data has been worked out")
-#for key,value in data.items():
-#    print([key, value])
-
-##################################
-#print the data
-with open("NPI_bg.csv", "a") as output:
+with open("NPI_bg.csv", "w") as output:
     writer = csv.writer(output)
+    with open("F:/Academic/Stat 992/group project/"
+              "PartD_Prescriber_PUF_NPI_DRUG_13/PARTD_PRESCRIBER_PUF_NPI_DRUG_13.tab") as file:
+    #with open("F:/Academic/Stat 992/group project/toy_data/wi_dem2.tab") as file:
+        rt.extend(file.readlines())
+        ### Write the file
+        for i in range(len(rt)):
+            rows = rt[i].strip().split("\t")
+            if(isInt(rows[0])):
+                if rows[0] in data:  # if the physician is in the dictionary created
+                    data[rows[0]][1] += 1
+                else: # create a new dictionary for this physician
+                    data[rows[0]] = [0, 1, 0, int(rows[10])] # add total_claim to general count
+                    data[rows[0]].extend(rows)
+                if rows[7] == rows[8]: # physician use a brand_name
+                    data[rows[0]][0] += 1
+                    data[rows[0]][2] += int(rows[10])   ## add total_claim to brand-name count
+            else:
+                if(len(rows) > 8):
+                    ##### Write the header
+                    head = ["b/g"]
+                    head.append("b/g with claim")
+                    head.extend(rows)
+                    head.pop(10)
+                    head.pop(9)
+                    writer.writerows([head])
+    print("The data has been worked out")
+    #for key,value in data.items():
+    #    print([key, value])
+
+    ##################################
+    #print the data
+
     for i in data:
-        data[i][0] = data[i][0]/data[i][1]
-        data[i].pop(1)
-        data[i].pop(8)
-        data[i].pop(8)
+        data[i].pop(12) # delete the brand_name column
+        data[i].pop(11) # delete the generic_name column
+        data[i][0] = data[i][0]/data[i][1]  # calculate b/g
+        data[i][1] = data[i][2]/data[i][3]  # calculate b/g with total_claim
+        data[i][12] = data[i][3]
+        data[i].pop(3)
+        data[i].pop(2)
         writer.writerows([data[i]])
 
 print("The data has been written to NPI_bg.csv")
